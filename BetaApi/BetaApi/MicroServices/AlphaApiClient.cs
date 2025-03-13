be1614864acedb4f911e8010b29adc56706971cc
Alex <119462299+RaphaelsHub@users.noreply.github.com>
2025-03-13 11:06:39 +0200

    Changed name server on docker container's name.

--- BetaApi/BetaApi/MicroServices/AlphaApiClient.cs
+++ BetaApi/BetaApi/MicroServices/AlphaApiClient.cs
@@ -6,28 +6,28 @@ public class AlphaApiClient(HttpClient httpClient)
 {
     public async Task<string> GetDataFromAlphaApi()
     {
-        var response = await httpClient.GetAsync("http://localhost:5211/alphaApi/data");
+        var response = await httpClient.GetAsync("http://alpha-api-container/alphaApi/data");
         response.EnsureSuccessStatusCode();
         return await response.Content.ReadAsStringAsync();
     }
 
     public async Task<string> PostDataToAlphaApi(Data data)
     {
-        var response = await httpClient.PostAsJsonAsync("http://localhost:5211/alphaApi/data", data);
+        var response = await httpClient.PostAsJsonAsync("http://alpha-api-container/alphaApi/data", data);
         response.EnsureSuccessStatusCode();
         return await response.Content.ReadAsStringAsync();
     }
 
     public async Task<string> PutDataToAlphaApi(int id, Data data)
     {
-        var response = await httpClient.PutAsJsonAsync($"http://localhost:5211/alphaApi/data/{id}", data);
+        var response = await httpClient.PutAsJsonAsync($"http://alpha-api-container/alphaApi/data/{id}", data);
         response.EnsureSuccessStatusCode();
         return await response.Content.ReadAsStringAsync();
     }
 
     public async Task<string> DeleteDataFromAlphaApi(int id)
     {
-        var response = await httpClient.DeleteAsync($"http://localhost:5211/alphaApi/data/{id}");
+        var response = await httpClient.DeleteAsync($"http://alpha-api-container/alphaApi/data/{id}");
         response.EnsureSuccessStatusCode();
         return await response.Content.ReadAsStringAsync();
     }
